03cbd234f8fba6821500a885417f79ce53224bce
Your Name <you@example.com>
2022-07-20 19:11:29 -0400

    1.2

--- src/pacoteDeProdutos.js
+++ src/pacoteDeProdutos.js
@@ -1,25 +1,25 @@
 export const pacoteDeProdutos = [
     {
       id: 1,
-      name: 'Montanhas',
+      name: 'produto 4',
       price: 123,
       photo: 'https://picsum.photos/200/200?a=1',
     },
     {
       id: 2,
-      name: 'Mato',
+      name: 'produto 3',
       price: 200,
       photo: 'https://picsum.photos/200/200?a=2'
     },
     {
       id: 3,
-      name: 'Estrada',
+      name: 'produto 1',
       price: 30,
       photo: 'https://picsum.photos/200/200?a=3'
     },
     {
       id: 4,
-      name: 'Metro',
+      name: 'produto 2',
       price: 10,
       photo: 'https://picsum.photos/200/200?a=4'
     },
